9aa6492d51bfcbe062503c2606afbc86edf2fe37
Kill Toxic <913087364@qq.com>
2020-03-13 14:27:09 +0800

    camCaptureToGray

--- camCaptureToGray/camCaptureToGray.cpp
+++ camCaptureToGray/camCaptureToGray.cpp
@@ -1,20 +1,35 @@
-﻿// camCaptureToGray.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	VideoCapture cap(0);//调用摄像头
+	double scale = 0.5;//对取得的帧进行缩放
+	//0-100
+	//肤色
+	double i_minH = 0;
+	double i_maxH = 20;
+	//0-255
+	double i_minS = 43;
+	double i_maxS = 255;
+	//0-255
+	double i_minV = 55;
+	double i_maxV = 255;
+	//0-255
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat hsvMat;
+		cv::Mat detectMat;
+		cap >> frame;//捕捉当前帧
+		cvtColor(frame, hsvMat, COLOR_BGR2HSV);//转化为HSV格式
+		frame.copyTo(detectMat);
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);//保存到detectMat中
+		cv::imshow("white:in the rage", detectMat);//显示肤色捕捉帧
+		cv::imshow("frame", frame);//显示当前帧
+		waitKey(30);
+	}
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
